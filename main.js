a70d3715ceef4166defa93dcebb82844c3b6797b
mart-anthony-stark <martsalazar0@gmail.com>
2022-06-24 13:39:58 +0800

    Add: animatetion frame

--- main.js
+++ main.js
@@ -3,5 +3,15 @@ road.height = window.innerHeight;
 road.width = 500;
 
 const context = road.getContext("2d");
-const car = new Car(250, 200, 30, 50);
+const car = new Car(window.innerWidth / 2, window.innerHeight - 80, 40, 80);
 car.draw(context);
+
+animate();
+
+function animate() {
+  car.update();
+  road.height = window.innerHeight;
+  road.width = window.innerWidth;
+  car.draw(context);
+  requestAnimationFrame(animate);
+}
