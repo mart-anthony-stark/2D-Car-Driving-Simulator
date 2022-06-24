40aa6cb5828094e56b8395727fb0aeebcee3fb81
mart-anthony-stark <martsalazar0@gmail.com>
2022-06-24 14:16:47 +0800

    Add: main ts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
-import { Car } from "./utils";
+// import { Car } from "./utils";
+/// <reference path="./utils.ts" />
 
 const road = <HTMLCanvasElement>document.querySelector("canvas");
 road.height = window.innerHeight;
 road.width = 500;
 
 const context = road.getContext("2d");
-const car: Car = new Car(
+const car: util.Car = new util.Car(
   window.innerWidth / 2,
   window.innerHeight - 80,
   40,
